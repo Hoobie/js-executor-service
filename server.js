8f1c4e79303dd176825bb5ad64f83370c5af0533
Hoobie <hoobie92@gmail.com>
2016-11-26 15:02:06 +0100

    Add basic arguments handling

--- server.js
+++ server.js
@@ -12,17 +12,28 @@ const server = http.createServer(function(req, res) {
     req.on('data', function(chunk) {
         body.push(chunk);
     }).on('end', function() {
-        body = Buffer.concat(body).toString();
-        console.log('Received code: ' + body);
+        var json = JSON.parse(Buffer.concat(body).toString());
+        console.log('Received code: ' + json.code);
+        console.log('Received arguments: ' + json.args);
 
-        const result = vm.runInNewContext('f = ' + body + '; r = f();', sandbox);
+        var vmCode = 'f = ' + json.code + '; r = f(' + args(json) + ');';
+        const result = vm.runInNewContext(vmCode, sandbox);
         console.log('Computed: ' + result);
 
         res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Content-Type', 'text/plain');
+        res.setHeader('Content-Type', 'text/json');
         res.writeHead(200);
-        res.end(sandbox.r);
+        res.end(sandbox.r.toString());
     });
 });
 
 server.listen(port);
+
+function args(obj) {
+  var arguments = '';
+  for (i = 0; i < obj.args.length; i++) {
+    console.log(obj.args[i]);
+    arguments += obj.args[i] + ', ';
+  }
+  return arguments.substring(0, arguments.length - 2);
+}
